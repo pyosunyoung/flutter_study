61cba1c8254562aa5a6371306901b83f80aa1a5d
pyosunyoung <tjsdudtbvj12@naver.com>
2025-12-30 23:08:02 +0900

    feat: 좋아요, 피드 리스트 구현(ListView.builder 활용)

--- lib/feed.dart
+++ lib/feed.dart
@@ -2,16 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 //자주 사용되는 body부분 Feed로 따로 뺴줌 재사용성 증가, extract widget 전구 아니면 컨트롤 + 시프트 + R 하면 됨.
-class Feed extends StatefulWidget { // stateless => stateful로 변경 ctrl .상태가 변하기 때문
+class Feed extends StatefulWidget {
+  // stateless => stateful로 변경 ctrl .상태가 변하기 때문
   const Feed({
     super.key,
-  });
+    required this.imageUrl, //this는 자기 자신을 가리킴
+  }); //Feed(생성자)가 호출될 때 imageUrl에 값을 할당해줄 것.
+
+  final String imageUrl; // 이미지를 담을 변수
 
   @override
   State<Feed> createState() => _FeedState();
 }
 
+//_함수는 이 파일 내부에서만 접근가능한 함수. 즉 외부에서 imageUrl은 위쪽에 값이 삽입 되어 아래 _함수에 imageUrl값이 내려올 것.
 class _FeedState extends State<Feed> {
+  // 좋아요 여부
+  bool isFavorite = false;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -19,7 +27,7 @@ class _FeedState extends State<Feed> {
       children: [
         // 이미지
         Image.network(
-          "https://devclass.devstory.co.kr/flutter-basic/2/cat.png",
+          widget.imageUrl,
           height: 400,
           width: double.infinity,
           fit: BoxFit.cover,
@@ -27,8 +35,17 @@ class _FeedState extends State<Feed> {
         Row(
           children: [
             IconButton(
-              icon: Icon(CupertinoIcons.heart, color: Colors.black),
-              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.heart,
+                color: isFavorite ? Colors.pink : Colors.black,
+              ),
+              onPressed: () {
+                setState(() {
+                  // setState를 추가해야 화면이 갱신되어서 넣어줘야 함.
+                  //setState는 build를 다시 갱신 즉 재실행 해주는 역할.
+                  isFavorite = !isFavorite;
+                });
+              },
             ),
             IconButton(
               icon: Icon(CupertinoIcons.chat_bubble, color: Colors.black),
@@ -41,16 +58,13 @@ class _FeedState extends State<Feed> {
             ),
           ],
         ),
-    
+
         // 좋아요
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Text(
-            "2 likes",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
+          child: Text("2 likes", style: TextStyle(fontWeight: FontWeight.bold)),
         ),
-    
+
         // 설명
         Padding(
           padding: const EdgeInsets.all(8.0),
@@ -58,7 +72,7 @@ class _FeedState extends State<Feed> {
             "My cat is docile even when bathed. I put a duck on his head in the wick and he's staring at me. Isn't it so cute??",
           ),
         ),
-    
+
         // 날짜
         Padding(
           padding: const EdgeInsets.all(8.0),
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,6 +7,15 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> images = [
+      "https://devclass.devstory.co.kr/flutter-basic/2/insta-cat1.jpg",
+      "https://devclass.devstory.co.kr/flutter-basic/2/insta-cat2.jpg",
+      "https://devclass.devstory.co.kr/flutter-basic/2/insta-cat3.jpg",
+      "https://devclass.devstory.co.kr/flutter-basic/2/insta-cat4.jpg",
+      "https://devclass.devstory.co.kr/flutter-basic/2/insta-cat5.jpg",
+      "https://devclass.devstory.co.kr/flutter-basic/2/insta-cat6.jpg",
+      "https://devclass.devstory.co.kr/flutter-basic/2/insta-cat7.gif",
+    ];
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -25,8 +34,17 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Feed(),
+      body: ListView.builder(
+        //listView는 item 100개가 있을 때 한번에 100개를 빌드하지 않고 화면에 보이는 것부터 아이템을 랜더링함.
+        // 즉 스크롤을 내릴 때 마다 동적으로 데이터를 불러와서 최적화하는 기능임.
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          String image = images[index];
+          return Feed(imageUrl: image);
+        },
+      ),
     );
   }
 }
+
 //자주 사용되는 body부분 Feed로 따로 뺴줌 재사용성 증가, extract widget 전구 아니면 컨트롤 + 시프트 + R 하면 됨.
