ff63f3afdc097964bea4d51c0287e28d05b4ea00
pyosunyoung <tjsdudtbvj12@naver.com>
2026-01-13 15:21:12 +0900

    feat: 좋아요 페이지 shared_preferences 적용

--- random_cat/lib/main.dart
+++ random_cat/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  // main() 함수에서 async를 쓰려면 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => CatService()),
+        ChangeNotifierProvider(create: (context) => CatService(prefs)),
       ], // 생성자 호출
       child: const MyApp(),
     ),
@@ -28,9 +35,18 @@ class CatService extends ChangeNotifier {
   List<String> catImages = [];
 
   List<String> favoriteImages = [];
-  CatService() {
+
+  // SharedPreferences 인스턴스
+  SharedPreferences prefs;
+
+  //this.prefs라고 적어주면 처음 생성자를 호출할 때 SharedPreferences를 전달 받아 prefs 속성에 바로 할당해줍니다.
+  CatService(this.prefs) {
     //생성자
     getRandomCatImages();
+    //생성자가 호출되는 시점에서 실행되도록 설정.
+    // favorites로 저장된 favoriteImages를 가져옵니다.
+    // 저장된 값이 없는 경우 null을 반환하므로 이때는 빈 배열을 넣어줍니다.
+    favoriteImages = prefs.getStringList("favorites") ?? [];
   }
 
   //랜덤 고양이 사진 API 호출
@@ -54,6 +70,12 @@ class CatService extends ChangeNotifier {
       favoriteImages.add(catImage); // 좋아요 이미지 추가
     }
 
+    // favoriteImages를 favorites라는 이름으로 저장하기
+    prefs.setStringList(
+      "favorites",
+      favoriteImages,
+    ); //문자열 배열을 저장할 때 쓰는 함수(setStringList)
+    //favorites라는 이름으로 저장 => 생성자 부분에서 get으로 가져옴
     notifyListeners(); // consumer아래 builder부분 새로 시작해서 갱신
   }
 }
