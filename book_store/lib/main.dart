152d745072b551bb260a97f7fd1c9bed1aecc717
pyosunyoung <tjsdudtbvj12@naver.com>
2026-01-14 10:50:57 +0900

    feat: Books API를 활용한 Book Store 앱 검색 기능 구현 및 url_launcher로 상세 페이지 연결

---
+++ book_store/lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:book_store/book.dart';
+import 'package:book_store/book_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => BookService())],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  /// 검색어를 가져올 수 있도록 TextField와 연결해 줍니다.
+  final TextEditingController searchController = TextEditingController();
+
+  void search(BookService bookservice) {
+    String keyword = searchController.text;
+    if (keyword.isNotEmpty) {
+      bookservice.getBookList(keyword);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<BookService>(
+      builder: (context, bookService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Book Store",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            actions: [
+              Container(
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.only(right: 12),
+                child: Text(
+                  "total ${bookService.bookList.length}",
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+            ],
+
+            /// AppBar의 Bottom은 항상 PreferredSize 위젯으로 시작해야합니다.
+            bottom: PreferredSize(
+              preferredSize: Size(double.infinity, 72),
+              child: Padding(
+                padding: EdgeInsetsGeometry.all(8),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    hintText: "원하시는 책을 검색해주세요",
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: () {
+                        //돋보기 아이콘 클릭
+                        search(bookService);
+                      },
+                    ),
+                  ),
+                  onSubmitted: (value) {
+                    //엔터 누르는 경우
+                    search(bookService);
+                  },
+                ),
+              ),
+            ),
+          ),
+
+          body: bookService.bookList.isEmpty
+              // bookList가 비어있는 경우
+              ? Center(
+                  child: Text(
+                    "검색어를 입력해 주세요",
+                    style: TextStyle(fontSize: 21, color: Colors.grey),
+                  ),
+                )
+              // bookList 보여주기
+              : ListView.builder(
+                  itemCount: bookService.bookList.length,
+                  itemBuilder: (context, index) {
+                    Book book = bookService.bookList[index];
+                    return ListTile(
+                      leading: Image.network(
+                        book.thumbnail,
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(book.title),
+                      subtitle: Text(book.subtitle),
+                      onTap: () {
+                        // 클릭시 previewLink 띄우기
+                        Uri uri = Uri.parse(book.previewLink);
+                        launchUrl(uri);
+                      },
+                    );
+                  },
+                ),
+        );
+      },
+    );
+  }
+}
